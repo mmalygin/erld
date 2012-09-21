1406d7b5051f26ec31f654e25f78e3ea6a7a2762
Eric Merritt <eric.merritt@five9.com>
2012-09-21 13:08:15 -0700

    fix various mac oriented warnings

--- c_src/debug.h
+++ c_src/debug.h
@@ -1,20 +1,20 @@
 /*
-	erld - A UNIX-style daemon wrapper for Erlang
-	Copyright (C) 2012 ShoreTel Inc.
+        erld - A UNIX-style daemon wrapper for Erlang
+        Copyright (C) 2012 ShoreTel Inc.
 
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
+        This program is free software; you can redistribute it and/or
+        modify it under the terms of the GNU General Public License
+        as published by the Free Software Foundation; either version 2
+        of the License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 /* See source file for description */
@@ -34,9 +34,9 @@ extern int ei_tracelevel; // Hook into the ei library debug
 #define DEBUG(FMT) debug(__FUNCTION__, FMT)
 #define DEBUG_V(FMT, ...) debug(__FUNCTION__, FMT, __VA_ARGS__)
 
-#define LOG(FMT) log(1, 1, __FUNCTION__, FMT)
-#define LOG_V(FMT, ...) log(1, 1, __FUNCTION__, FMT, __VA_ARGS__)
+#define LOG(FMT) erld_log(1, 1, __FUNCTION__, FMT)
+#define LOG_V(FMT, ...) erld_log(1, 1, __FUNCTION__, FMT, __VA_ARGS__)
 
 void fatal_error(const char *file, int line, int e, const char *fmt, ...) __attribute__((format(printf, 4, 5)));
-void log(int prefix, int suffix, const char *fun, const char *fmt, ...) __attribute__((format(printf, 4, 5)));
+void erld_log(int prefix, int suffix, const char *fun, const char *fmt, ...) __attribute__((format(printf, 4, 5)));
 void debug(const char *fun, const char *fmt, ...) __attribute__((format(printf, 2, 3)));
